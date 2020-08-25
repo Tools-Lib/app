f43f99ab0b9160bdef0d61db311caccc25937b03
0jxf <ahmad.ahmadpro2@gmail.com>
2020-08-25 02:05:25 +0200

    Edit client.js and remove from metas

--- src/requires/metas/default.php
+++ src/requires/metas/default.php
@@ -21,9 +21,9 @@
 
 	<!-- JS -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<!-- <script type="text/javascript" src="/public/js/client.js?s=<?php echo date('r', time());?>" defer></script> -->
 	<script type="text/javascript" src="/public/js/bootstrap.js"></script>
 
 	<!-- Other -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+	<noscript><style>div {display: none;}body {background-color: #000 !important}; </style><h1 class="text-center text-danger p-5 my-auto mx-auto bg-warning escrpt">This page needs JavaScript activated to work.</h1></noscript>
 	
